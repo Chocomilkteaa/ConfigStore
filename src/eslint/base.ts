ed7577f3b3dc309113bf79888ea11dc23cdf4e71
ChocoMilktea <milktea126421@gmail.com>
2026-08-19 09:10:49 +0000

    feat: add base ESLint configuration file for code linting

---
+++ src/eslint/base.ts
@@ -0,0 +1,32 @@
+import eslintJs from "@eslint/js";
+import typescriptEslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import globals from "globals";
+
+const config = defineConfig(
+  {
+    ignores: [
+      "node_modules/",
+    ],
+    extends: [
+      eslintJs.configs.recommended,
+      typescriptEslint.configs.strictTypeChecked,
+      typescriptEslint.configs.stylisticTypeChecked,
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.es2024,
+        ...globals.node,
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        projectService: true,
+      },
+    }
+  },
+  eslintConfigPrettier
+);
+
+export default config;
